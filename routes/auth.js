749d34efe2fafc6997ee77a42652f1bd4c4b13e7
Josmar Salvador <josmarmp96@gmail.com>
2021-05-13 18:32:28 -0500

    Section 8

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+/* 
+
+    path: api/login
+
+*/
+
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { crearUsuario, login, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+router.post('/new', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    check('email', 'El email no tiene el formato correcto example@example.com').isEmail().normalizeEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validarCampos
+] ,crearUsuario);
+
+router.post('/', [
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    check('email', 'El email no tiene el formato correcto example@example.com').isEmail().normalizeEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validarCampos
+], login );
+
+
+router.get('/renew', validarJWT, renewToken);
+
+
+
+
+
+
+module.exports = router;
