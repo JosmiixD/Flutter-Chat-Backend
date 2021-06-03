994bfb0524e11bab80b85fb94efae94799e6428f
Josmar Salvador <josmarmp96@gmail.com>
2021-06-03 12:57:21 -0500

    Socket Server Completed

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,20 +1,36 @@
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require('../controllers/socket')
 
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
 
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+    const [ valido, uid ] = comprobarJWT( client.handshake.headers['x-token'] );
+
+    //Verificar autenticacion
+    if( !valido ) { return client.disconnect(); }
+
+    //Cliente autenticado
+    usuarioConectado( uid );
+
+    //Ingresar al usuario a una sala en particular
+    //Sala global, cliente.id,
+    client.join( uid );
+
+
+    //Escuchar del cliente el mensaje personal
+
+    client.on('mensaje-personal', async ( payload ) => {
+        //TODO: Grabar mensaje
+        await grabarMensaje( payload );
+        io.to( payload.para ).emit('mensaje-personal', payload );
     });
 
-    // client.on('mensaje', ( payload ) => {
-    //     console.log('Mensaje', payload);
-
-    //     io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
-
-    // });
+    client.on('disconnect', () => {
+        usuarioDesconectado( uid );
+    });
 
 
 });
